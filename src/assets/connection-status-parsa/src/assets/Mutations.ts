5c2ba3297901e7c4aa50d421bfff7be52b130eb3
Farbod Mousavi <104089579+FarbodM7@users.noreply.github.com>
2024-11-02 16:10:45 +0330

    Delete src/assets/connection-status-parsa directory

--- src/assets/connection-status-parsa/src/assets/Mutations.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const UPDATE_CONNECTION_STATUS = gql`
-  mutation UpdateConnectionStatus($rtt: Float!) {
-    updateConnectionStatus(rtt: $rtt)
-  }
-`;
-
-export default { UPDATE_CONNECTION_STATUS };
--- src/assets/connection-status-parsa/src/assets/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-    uri: 'https://your-graphql-endpoint.com/graphql', // replace with graphql server url
-    cache: new InMemoryCache(),
-});
-
-export default client;
