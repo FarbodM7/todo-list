328f3c5f7b068a421a169b9ba207c501df67468a
Farbod Mousavi <104089579+FarbodM7@users.noreply.github.com>
2024-11-02 16:11:16 +0330

    Delete src/assets/src directory

--- src/assets/src/assets/apolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-    uri: 'https://your-graphql-endpoint.com/graphql', // must replace with GraphQL server URL
-    cache: new InMemoryCache(),
-});
-
-export default client;
